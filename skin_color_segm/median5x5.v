023bf56990f15a20b4f32f80d46ea0be2a0cfd0f
lsriw <lsriw@localhost.localdomain>
2015-05-14 19:21:27 +0200

    Zadanie 12.1 Dodany blok BRAM.

---
+++ skin_color_segm/median5x5.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:05:34 05/14/2015 
+// Design Name: 
+// Module Name:    median5x5 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module median5x5 # (
+	parameter [9:0] H_SIZE = 83
+)(
+		input binary,
+		input de,
+		input vsync,
+		input hsync,
+		
+		output median
+    );
+
+
+endmodule

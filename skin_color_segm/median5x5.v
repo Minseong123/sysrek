989ae4b3092750add8b063c3fd3397c35a917754
Wojtek Gumuła <voodkapl@gmail.com>
2015-05-18 21:17:34 +0200

    Zadanie 12.1 Implementacja symulacyjna układu filtracji medianowej z maską o rozmiarze 5x5.

--- skin_color_segm/median5x5.v
+++ skin_color_segm/median5x5.v
@@ -19,15 +19,134 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module median5x5 # (
-	parameter [9:0] H_SIZE = 83
+	parameter [9:0] H_SIZE = 83 // domylny rozmiar obrazu to szeroko = 64 piksele
 )(
-		input binary,
-		input de,
-		input vsync,
-		input hsync,
-		
-		output median
-    );
+	input clk,
+	input ce,
+	input rst,
+	
+	input mask,
+	input in_de,
+	input in_vsync,
+	input in_hsync,
+	
+	output median,
+	output out_de,
+	output out_vsync,
+	output out_hsync   
+	);
 
+wire [3:0] delay_line11;
+reg  [3:0] delay_line12 [4:0];
+wire [3:0] delay_line21;
+reg  [3:0] delay_line22 [4:0];
+wire [3:0] delay_line31;
+reg  [3:0] delay_line32 [4:0];
+wire [3:0] delay_line41;
+reg  [3:0] delay_line42 [4:0];
+wire [3:0] delay_line51;
+reg  [3:0] delay_line52 [4:0];
+
+reg [4:0] sum_lines[4:0];
+reg [4:0] sum_final;
+assign delay_line11 = {mask, in_de, in_hsync, in_vsync};
+
+//pojedyncze opoznienia dla piksela
+genvar i;
+generate
+		always @(posedge clk)
+		begin
+				delay_line12[0] <= delay_line11;
+				delay_line22[0] <= delay_line21;
+				delay_line32[0] <= delay_line31;
+				delay_line42[0] <= delay_line41;
+				delay_line52[0] <= delay_line51;
+				
+				delay_line12[1] <= delay_line12[0];
+				delay_line22[1] <= delay_line22[0];
+				delay_line32[1] <= delay_line32[0];
+				delay_line42[1] <= delay_line42[0];
+				delay_line52[1] <= delay_line52[0];
+				
+				delay_line12[2] <= delay_line12[1];
+				delay_line22[2] <= delay_line22[1];
+				delay_line32[2] <= delay_line32[1];
+				delay_line42[2] <= delay_line42[1];
+				delay_line52[2] <= delay_line52[1];
+				
+				delay_line12[3] <= delay_line12[2];
+				delay_line22[3] <= delay_line22[2];
+				delay_line32[3] <= delay_line32[2];
+				delay_line42[3] <= delay_line42[2];
+				delay_line52[3] <= delay_line52[2];
+				
+				delay_line12[4] <= delay_line12[3];
+				delay_line22[4] <= delay_line22[3];
+				delay_line32[4] <= delay_line32[3];
+				delay_line42[4] <= delay_line42[3];
+				delay_line52[4] <= delay_line52[3];
+
+	end
+endgenerate;
+
+
+delayLinieBRAM_WP long_delay
+(    
+	.clk(clk),
+	.rst(1'b0),
+	.ce(1'b1),
+	.din({delay_line12[4], delay_line22[4], delay_line32[4], delay_line42[4]}),
+	.dout({delay_line21, delay_line31, delay_line41, delay_line51}),
+	.h_size(H_SIZE - 5)
+);
+
+always @(posedge clk)
+begin
+	//wyznaczanie sum
+	if(context_valid) begin
+		sum_lines[0] <= delay_line12[0][3] + delay_line12[1][3] + delay_line12[2][3] + delay_line12[3][3] + delay_line12[4][3];
+		sum_lines[1] <= delay_line22[0][3] + delay_line22[1][3] + delay_line22[2][3] + delay_line22[3][3] + delay_line22[4][3];
+		sum_lines[2] <= delay_line32[0][3] + delay_line32[1][3] + delay_line32[2][3] + delay_line32[3][3] + delay_line32[4][3];
+		sum_lines[3] <= delay_line42[0][3] + delay_line42[1][3] + delay_line42[2][3] + delay_line42[3][3] + delay_line42[4][3];
+		sum_lines[4] <= delay_line52[0][3] + delay_line52[1][3] + delay_line52[2][3] + delay_line52[3][3] + delay_line52[4][3];	
+	
+		sum_final <= sum_lines[0] + sum_lines[1] + sum_lines[2] + sum_lines[3] + sum_lines[4];
+	end
+end
+
+wire context_valid;
+
+assign context_valid = delay_line12[0][1] & delay_line12[1][1] & delay_line12[2][1] & delay_line12[3][1] & delay_line12[4][1] & 
+	delay_line22[0][1] & delay_line22[1][1] & delay_line22[2][1] & delay_line22[3][1] & delay_line22[4][1] & 
+	delay_line32[0][1] & delay_line32[1][1] & delay_line32[2][1] & delay_line32[3][1] & delay_line32[4][1] & 
+	delay_line42[0][1] & delay_line42[1][1] & delay_line42[2][1] & delay_line42[3][1] & delay_line42[4][1] & 
+	delay_line52[0][1] & delay_line52[1][1] & delay_line52[2][1] & delay_line52[3][1] & delay_line52[4][1];
+
+
+wire [2:0] d_dhv;
+
+delay #
+(
+	.N(3),
+	.DELAY(2)
+)
+dhv
+(
+	.d(delay_line32[3][2:0]),
+	.ce(1'b1),
+	.clk(clk),
+	.q({d_dhv})
+);
+
+assign out_de = d_dhv[2];
+assign out_hsync = d_dhv[1];
+assign out_vsync = d_dhv[0];
+
+reg median_latch = 0;
+always @(posedge clk)
+begin
+	if(context_valid) median_latch <= (sum_final > 5'd12) ? 1'b1 : 1'b0;
+end;
+assign median = median_latch;
 
 endmodule
---
+++ skin_color_segm/tb_median5x5.v
@@ -0,0 +1,121 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:14:28 05/18/2015
+// Design Name:   median5x5
+// Module Name:   /home/vka/Programming/VHDL/workspace/sysrek/skin_color_segm/tb_median5x5.v
+// Project Name:  vision
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: median5x5
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb_median5x5(
+    );
+	 
+
+wire rx_pclk;
+
+wire rx_de;
+wire rx_hsync;
+wire rx_vsync;
+
+wire [7:0] rx_red;
+wire [7:0] rx_green;
+wire [7:0] rx_blue;
+
+
+wire tx_de;
+wire tx_hsync;
+wire tx_vsync;
+
+wire [7:0] tx_red;
+wire [7:0] tx_green;
+wire [7:0] tx_blue;
+
+
+
+// --------------------------------------
+// HDMI input
+// --------------------------------------
+hdmi_in file_input (
+    .hdmi_clk(rx_pclk), 
+    .hdmi_de(rx_de), 
+    .hdmi_hs(rx_hsync), 
+    .hdmi_vs(rx_vsync), 
+    .hdmi_r(rx_red), 
+    .hdmi_g(rx_green), 
+    .hdmi_b(rx_blue)
+    );
+
+// proccessing 
+	 
+reg [7:0] median_r;
+reg [7:0] median_g;
+reg [7:0] median_b;
+
+wire median;
+wire median_de;
+wire median_vsync;
+wire median_hsync;
+
+median5x5 #
+(
+	.H_SIZE(10'd83)
+)
+med5
+(
+	.clk(rx_pclk),
+	.ce(1'b1),
+	.rst(1'b0),
+
+	.mask((rx_red == 8'hFF) ? 1'b1 : 1'b0),
+	.in_de(rx_de),
+	.in_vsync(rx_vsync),
+	.in_hsync(rx_hsync),
+
+	.median(median),
+	.out_de(median_de),
+	.out_vsync(median_vsync),
+	.out_hsync(median_hsync)
+
+);
+
+always @(posedge rx_pclk) begin
+	median_r = (median) ? 8'hFF : 8'h00;
+	median_g = (median) ? 8'hFF : 8'h00;
+	median_b = (median) ? 8'hFF : 8'h00;
+end
+// --------------------------------------
+// Output assigment
+// --------------------------------------
+  
+	assign tx_de 				= median_de;
+	assign tx_hsync 			= median_hsync;
+	assign tx_vsync 			= median_vsync;
+	assign tx_red         	= median_r;
+	assign tx_green        	= median_g;
+	assign tx_blue         	= median_b;
+
+// --------------------------------------
+// HDMI output
+// --------------------------------------
+hdmi_out file_output (
+    .hdmi_clk(rx_pclk), 
+    .hdmi_vs(tx_vsync), 
+    .hdmi_de(tx_de), 
+    .hdmi_data({8'b0,tx_red,tx_green,tx_blue})
+    );
+endmodule

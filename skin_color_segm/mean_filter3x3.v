506fb752ffec7c702f877768d3dd488273daddef
Wojtek Gumuła <voodkapl@gmail.com>
2015-05-19 19:42:59 +0200

    Zadanie 12.3 Moduł filtracji uśredniającej. (Wersja to sprawdzenia)

---
+++ skin_color_segm/mean_filter3x3.v
@@ -0,0 +1,168 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Wojtek Gumua
+// 
+// Create Date:    19:16:36 05/19/2015 
+// Design Name: 
+// Module Name:    mean_filter3x3 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mean_filter3x3 #(
+    	parameter [9:0] H_SIZE = 83 // domylny rozmiar obrazu to szeroko = 64 piksele
+)(
+	input clk,
+	input ce,
+	input rst,
+	
+	input [7:0] in_Y,
+	input [7:0] in_Cb,
+	input [7:0] in_Cr,
+	input in_de,
+	input in_vsync,
+	input in_hsync,
+	
+	output [7:0] out_Y,
+	output [7:0] out_Cb,
+	output [7:0] out_Cr,
+	output out_de,
+	output out_vsync,
+	output out_hsync   
+	);
+	
+wire [10:0] delay_line11;
+reg  [10:0] delay_line12 [2:0];
+wire [10:0] delay_line21;
+reg  [10:0] delay_line22 [2:0];
+wire [10:0] delay_line31;
+reg  [10:0] delay_line32 [2:0];
+
+reg [11:0] sum_lines[2:0];
+reg [11:0] sum_final;
+assign delay_line11 = {in_Y, in_de, in_hsync, in_vsync};
+
+//pojedyncze opoznienia dla piksela
+genvar i;
+generate
+		always @(posedge clk)
+		begin
+				delay_line12[0] <= delay_line11;
+				delay_line22[0] <= delay_line21;
+				delay_line32[0] <= delay_line31;
+				
+				delay_line12[1] <= delay_line12[0];
+				delay_line22[1] <= delay_line22[0];
+				delay_line32[1] <= delay_line32[0];
+				
+				delay_line12[2] <= delay_line12[1];
+				delay_line22[2] <= delay_line22[1];
+				delay_line32[2] <= delay_line32[1];
+	end
+endgenerate;
+
+wire [9:0] dummy;//zbdne dane
+delayLinieBRAM_WP long_delay1
+(    
+	.clk(clk),
+	.rst(1'b0),
+	.ce(1'b1),
+	.din({5'b0, delay_line12[2]}),
+	.dout({dummy[4:0], delay_line21}),
+	.h_size(H_SIZE - 3)
+);
+
+delayLinieBRAM_WP long_delay2
+(    
+	.clk(clk),
+	.rst(1'b0),
+	.ce(1'b1),
+	.din({5'b0, delay_line22[2]}),
+	.dout({dummy[9:5], delay_line31}),
+	.h_size(H_SIZE - 3)
+);
+
+always @(posedge clk)
+begin
+	//wyznaczanie sum
+	if(context_valid) begin
+		//A + 2B + C
+		sum_lines[0] <= {4'b0, delay_line12[0][10:3]} + {3'b0, delay_line12[1][10:3], 1'b0} + {4'b0,delay_line12[2][10:3]};
+		//2A + 4B + 2B
+		sum_lines[1] <= {3'b0, delay_line22[0][10:3], 1'b0} + {2'b0, delay_line22[1][10:3], 2'b0} + {3'b0, delay_line22[2][10:3], 1'b0};
+		//A + 2B + C
+		sum_lines[2] <= {4'b0, delay_line32[0][10:3]} + {3'b0, delay_line32[1][10:3], 1'b0} + {4'b0,delay_line32[2][10:3]};
+	
+		sum_final <= sum_lines[0] + sum_lines[1] + sum_lines[2];
+	end
+end
+
+wire context_valid;
+
+assign context_valid = delay_line12[0][1] & delay_line12[1][1] & delay_line12[2][1] & 
+	delay_line22[0][1] & delay_line22[1][1] & delay_line22[2][1] & 
+	delay_line32[0][1] & delay_line32[1][1] & delay_line32[2][1];
+
+wire [2:0] d_dhv;
+
+delay #
+(
+	.N(3),
+	.DELAY(2)
+)
+dhv
+(
+	.d(delay_line22[2][2:0]),
+	.ce(1'b1),
+	.clk(clk),
+	.q({d_dhv})
+);
+
+assign out_de = d_dhv[2];
+assign out_hsync = d_dhv[1];
+assign out_vsync = d_dhv[0];
+
+reg [7:0] mean_latch = 0;
+
+always @(posedge clk)
+begin
+	if(context_valid) mean_latch <= sum_final[11:4];
+end;
+assign out_Y = mean_latch;
+
+//opoznienie sygnalow Cb, Cr
+delay #
+(
+	.N(8),
+	.DELAY(2 + H_SIZE + 1)
+)
+delay_cb
+(
+	.d(in_Cb),
+	.ce(1'b1),
+	.clk(clk),
+	.q(out_Cb)
+);
+delay #
+(
+	.N(8),
+	.DELAY(2 + H_SIZE + 1)
+)
+delay_cr
+(
+	.d(in_Cr),
+	.ce(1'b1),
+	.clk(clk),
+	.q(out_Cr)
+);
+
+endmodule
---
+++ skin_color_segm/tb_mean_filter3x3.v
@@ -0,0 +1,141 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:29:00 05/19/2015
+// Design Name:   mean_filter3x3
+// Module Name:   /home/vka/Programming/VHDL/workspace/sysrek/skin_color_segm/tb_mean_filter3x3.v
+// Project Name:  vision
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: mean_filter3x3
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb_mean_filter3x3(
+    );
+	 
+
+wire rx_pclk;
+
+wire rx_de;
+wire rx_hsync;
+wire rx_vsync;
+
+wire [7:0] rx_red;
+wire [7:0] rx_green;
+wire [7:0] rx_blue;
+
+
+wire tx_de;
+wire tx_hsync;
+wire tx_vsync;
+
+wire [7:0] tx_red;
+wire [7:0] tx_green;
+wire [7:0] tx_blue;
+
+
+
+// --------------------------------------
+// HDMI input
+// --------------------------------------
+hdmi_in file_input (
+    .hdmi_clk(rx_pclk), 
+    .hdmi_de(rx_de), 
+    .hdmi_hs(rx_hsync), 
+    .hdmi_vs(rx_vsync), 
+    .hdmi_r(rx_red), 
+    .hdmi_g(rx_green), 
+    .hdmi_b(rx_blue)
+    );
+
+// proccessing 
+wire [7:0] conv_Y;
+wire [7:0] conv_Cb;
+wire [7:0] conv_Cr;
+wire conv_de;
+wire conv_vsync;
+wire conv_hsync;
+
+wire [7:0] mean_Y;
+wire [7:0] mean_Cb;
+wire [7:0] mean_Cr;
+wire mean_de;
+wire mean_vsync;
+wire mean_hsync;
+
+rgb2ycbcr conv (
+	.clk(rx_pclk), 
+	.ce(1'b1), 
+	.R(rx_red), 
+	.G(rx_green), 
+	.B(rx_blue), 
+	.in_hsync(rx_hsync), 
+	.in_vsync(rx_vsync), 
+	.in_de(rx_de), 
+	.Y(conv_Y), 
+	.Cb(conv_Cb), 
+	.Cr(conv_Cr), 
+	.out_hsync(conv_hsync), 
+	.out_vsync(conv_vsync), 
+	.out_de(conv_de)
+);
+
+mean_filter3x3 #
+(
+	.H_SIZE(10'd83)
+)
+mean_f
+(
+	.clk(rx_pclk),
+	.ce(1'b1),
+	.rst(1'b0),
+
+	.in_Y(conv_Y),
+	.in_Cb(conv_Cb),
+	.in_Cr(conv_Cr),
+	.in_de(conv_de),
+	.in_vsync(conv_vsync),
+	.in_hsync(conv_hsync),
+	
+	.out_Y(mean_Y),
+	.out_Cb(mean_Cb),
+	.out_Cr(mean_Cr),
+	.out_de(mean_de),
+	.out_vsync(mean_vsync),
+	.out_hsync(mean_hsync)   
+
+);
+
+// --------------------------------------
+// Output assigment
+// --------------------------------------
+  
+	assign tx_de 				= mean_de;
+	assign tx_hsync 			= mean_hsync;
+	assign tx_vsync 			= mean_vsync;
+	assign tx_red         	= mean_Y;
+	assign tx_green        	= mean_Cb;
+	assign tx_blue         	= mean_Cr;
+
+// --------------------------------------
+// HDMI output
+// --------------------------------------
+hdmi_out file_output (
+    .hdmi_clk(rx_pclk), 
+    .hdmi_vs(tx_vsync), 
+    .hdmi_de(tx_de), 
+    .hdmi_data({8'b0,tx_red,tx_green,tx_blue})
+    );
+endmodule
